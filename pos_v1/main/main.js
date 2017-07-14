f068338e932f34b661730ee1d69a3d50cd6407a4
fengzi2016 <1820166258@qq.com>
2017-07-14 15:23:30 +0800

    ok

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,12 +1,12 @@
 'use strict';
 function FizzBuzzWhizz(num){
-  let elements=[];
-  let num1=num;
-  while(num1) {
-    elements.push(num1 % 10);
-    num1 /= 10;
+  let elements = [];
+  let num1 = num;
+  while (num1>0) {
+    elements.push(parseInt(num1 % 10));
+    num1=parseInt(num1/10);
   }
-  if(elements.indexOf(3)>=0&&num%5!==0&&num%7!==0){
+  if(elements.indexOf(3)>=0){
     return 'Fizz';
   }else if(num%3===0){
     if(num%5===0&&num%7===0){
@@ -38,3 +38,28 @@ function FizzBuzzWhizz(num){
     return num;
   }
 }
+/*function  quchong(collection) {
+  let new_collection=[];
+  new_collection[0]={};
+  new_collection[0].barcode=collection[0];
+  new_collection[0].count=1;
+  let temp=[];
+  let k=0;
+  for(let i=1;i<collection.length;i++){
+    for(var j=0;j<new_collection.length;j++){
+      if(new_collection[j].barcode===collection[i])
+        break;
+    }
+    if(j===new_collection.length){
+      temp[k]={};
+      temp[k].barcode=collection[i];
+      temp[k].count=1;
+      new_collection.push(temp[k]);
+      k++;
+    }
+    else{
+      new_collection[j].count++;
+    }
+  }
+  return new_collection;
+}*/
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -2,56 +2,92 @@
  * Created by root on 17-7-14.
  */
 'use strict';
-describe('FizzBuzzWhizz',() => {
-    it( 'when num is times of 3' , ()=>{
-        let num=6;
-        let actualResult=FizzBuzzWhizz(num);
-        let expectedText='Fizz';
-        expect(actualResult).toEqual(expectedText);
+/*
+describe(
+  it('make a new array where there are values of "barcode" and "count"',() => {
+    const tag =
+      [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003',
+        'ITEM000005',
+        'ITEM000005',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001'
+      ];
+    let actualResult = quchong(tag);
+    let expectedText = [
+      {barcode: 'ITEM000001', count: 5},
+      {barcode: 'ITEM000003', count: 1},
+      {barcode: 'ITEM000005', count: 2}
+      ];
+    expect(actualResult).toEqual(expectedText);
+  })
+);
+*/
+describe('FizzzBuzzWhizz',()=>{
+    it('when num has element 3 and is times of 5 and is not times of 7',()=>{
+      let num=30;
+      let actualResult=FizzBuzzWhizz(num);
+      let expectedText='Fizz';
+      expect(actualResult).toEqual(expectedText);
     });
-    it('when num has element 3',()=>{
-        let num=13;
-        let actualResult=FizzBuzzWhizz(num);
-        let expectedText='Fizz';
-        expect(actualResult).toEqual(expectedText);
+    it('when num has element 3 and is times of 7 and is not times of 5',()=>{
+      let num=63;
+      let actualResult=FizzBuzzWhizz(num);
+      let expectedText='Fizz';
+      expect(actualResult).toEqual(expectedText);
     });
-    it('when num is times of 5',()=>{
+    it('when num has element 3 and is times of 5 and 7',()=>{
+       let num=35;
+       let actualResult=FizzBuzzWhizz(num);
+       let expectedText='Fizz';
+      expect(actualResult).toEqual(expectedText);
+    });
+    it( 'when num doesn"t have element 3 and is times of 3 and is not times of 5 or 7', ()=>{
+    let num=6;
+    let actualResult=FizzBuzzWhizz(num);
+    let expectedText='Fizz';
+    expect(actualResult).toEqual(expectedText);
+    });
+    it('when num doesn"t have element 3 and is times of 3 and 5 and is not times of 7',()=>{
+    let num=15;
+    let actualResult=FizzBuzzWhizz(num);
+    let expectedText='FizzBuzz';
+    expect(actualResult).toEqual(expectedText);
+    });
+    it('when num doesn"t have element 3 and is times of 3 and 7 and is not times of 5',()=>{
+    let num=21;
+    let actualResult=FizzBuzzWhizz(num);
+    let expectedText='FizzWhizz';
+    expect(actualResult).toEqual(expectedText);
+     });
+    it('when num doesn"t have element 3 and is times of 5',()=>{
         let num=10;
         let actualResult=FizzBuzzWhizz(num);
         let expectedText='Buzz';
         expect(actualResult).toEqual(expectedText);
     });
-    it('when num is times of 7',()=>{
+    it('when num doesn"t have element 3 and is times of 7 and is not times of 3 or 5 ',()=>{
         let num=14;
         let actualResult=FizzBuzzWhizz(num);
         let expectedText='Whizz';
         expect(actualResult).toEqual(expectedText);
     });
-    it('when num is times of 3 and 5',()=>{
-        let num=15;
-        let actualResult=FizzBuzzWhizz(num);
-        let expectedText='FizzBuzz';
-        expect(actualResult).toEqual(expectedText);
-    });
-    it('when num is times of 3 and 7',()=>{
-        let num=21;
-        let actualResult=FizzBuzzWhizz(num);
-        let expectedText='FizzWhizz';
-        expect(actualResult).toEqual(expectedText);
-    });
-    it('when num is times of 5 and 7',()=>{
-        let num=35;
+    it('when num doesn"t have element 3 and doesn"t have element 3 and is times of 5 and 7 and is not times of 3 ',()=>{
+        let num=70;
         let actualResult=FizzBuzzWhizz(num);
         let expectedText='BuzzWhizz';
         expect(actualResult).toEqual(expectedText);
     });
-    it('when num is times of 3 and 5 and 7 ',()=>{
+    it('when num doesn"t have element 3 and is times of 3 and 5 and 7 ',()=>{
         let num=105;
         let actualResult=FizzBuzzWhizz(num);
         let expectedText='FizzBuzzWhizz';
         expect(actualResult).toEqual(expectedText);
     });
-    it('when num is not times of 3 or 5 or 7 ',()=>{
+    it('when num doesn"t have element 3 and is not times of 3 or 5 or 7 ',()=>{
         let num=1;
         let actualResult=FizzBuzzWhizz(num);
         let expectedText=num;
